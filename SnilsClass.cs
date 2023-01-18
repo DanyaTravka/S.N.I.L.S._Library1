a56cb85a11972a4d12240399337f69cc2929a18a
rotoyouoi <rotoyouoi@yandex.ru>
2023-01-18 11:06:18 +0500

    Добавьте файлы проекта.

---
+++ SnilsClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snilsLibrary
+{
+    public class SnilsClass
+    {
+        public bool CheckMedicalCode(string textString)
+        {
+            var numbers = new List<int>();
+            string number = "987654321";
+            string snilsnumber = textString.Replace(" ", " ");
+            if(snilsnumber.Length == 11)
+            {
+                for (int i = 0; i < snilsnumber.Length - 2; i++)
+                {
+                    int b = snilsnumber[i] - number[i];
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
